c61acd06e67b637ed0ddc53edd66defedce2fa6c
unknown <vivekvelugubantla114@gmail.com>
2023-06-04 13:07:48 +0530

    Adding a new migration file that adds the role column to users table.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
     has_secure_password
 
+    enum role: {
+        default_user: 0,
+        super_admin: 10
+    }
+
     validates_presence_of :email
     validates_uniqueness_of :email
     has_many :lists
